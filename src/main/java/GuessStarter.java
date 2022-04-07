a0b595c92b2bd5309a3e112ee8c9f36a1a0d3ef6
LBEBEN <lukas.beben@gmail.com>
2022-04-07 13:38:32 +0200

    upgrade programu do zgadywani licz

--- src/main/java/GuessStarter.java
+++ src/main/java/GuessStarter.java
@@ -6,11 +6,11 @@ public class GuessStarter {
         Random random =new Random();
         int number = random.nextInt(100)+1;
         Scanner scanner = new Scanner(System.in);
-        int i;
+
 
         System.out.println("Myślę o liczbie pomiędzy 1 a 100\n"+"Czy zgadniesz jaka to liczba?");
         System.out.println("Podaj liczbę ");
-        i = scanner.nextInt();
+        int i = scanner.nextInt();
         System.out.println("Twoja propozycja to " + i);
         System.out.println("Liczba o której myślałem to " + number);
         if(i != number){
